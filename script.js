535b370f0edf290723808d6434aa3883c15bfd24
PaulusMetzger <47422236+PaulusMetzger@users.noreply.github.com>
2024-11-11 09:46:56 +0300

    Return

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 let flippedCards = [];
 let matchedCards = [];
-let cardsTotalAmount = 12;
 
 function flipCard(card) {
     if (flippedCards.length < 2 && !flippedCards.includes(card) && !matchedCards.includes(card)) {
         card.src = card.dataset.frontImage; // Переворачиваем картинку на переднюю сторону
         flippedCards.push(card);
-
         if (flippedCards.length === 2) {
             setTimeout(checkMatch, 1000); // Проверяем совпадение картинок после задержки
         }
@@ -14,22 +12,27 @@ function flipCard(card) {
 }
 
 function checkMatch() {
-    if (matchedCards.length === cardsTotalAmount) { // Проверка, что все карты совпали
-        showMainButn(); 
-    } else if (flippedCards[0].dataset.category === flippedCards[1].dataset.category) {
-        matchedCards.push(...flippedCards);
-        flippedCards = [];
+    if (flippedCards[0].dataset.category === flippedCards[1].dataset.category) {
+        matchedCards.push(...flippedCards); // Если картинки совпали, добавляем их в массив совпавших карт
+        flippedCards = []; // Очищаем массив открытых карт
     } else {
         flippedCards.forEach(flippedCard => {
-            flippedCard.src = flippedCard.dataset.image;
+            flippedCard.src = flippedCard.dataset.image; // Переворачиваем обратно несовпавшие картинки
         });
-        flippedCards = [];
+        flippedCards = []; // Очищаем массив открытых карт
     }
 }
 
-function showMainButn() {
-    tg.MainButton.show()
- }
+function restartGame() {
+    location.reload(); // Перезагрузка страницы для начала игры заново
+}
+// Получаем все элементы с классом "card"
+const cards = document.querySelectorAll('.card');
+// Случайно перемешиваем элементы "card" в родительском контейнере
+const gameGrid = document.querySelector('.game-grid');
+for (let i = gameGrid.children.length; i >= 0; i--) {
+    gameGrid.appendChild(gameGrid.children[Math.random() * i | 0]);
+}
 
 function restartGame() {
     location.reload(); // Перезагрузка страницы для начала игры заново
